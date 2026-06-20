d2484d337e372278dabb325549812a7f0578a792
Evan <136231429+luckievan@users.noreply.github.com>
2026-06-20 20:30:20 +0800

    chore: upgrade AXTP Spec to spec/v0.8.5 (#9)

--- include/generated/axtp_generated_version.h
+++ include/generated/axtp_generated_version.h
@@ -3,12 +3,12 @@
 #define AXTP_GENERATED_VERSION_H
 
 #define AXTP_RUNTIME_NAME "axtp-c-runtime"
-#define AXTP_RUNTIME_VERSION "0.8.4"
-#define AXTP_SPEC_VERSION "0.8.4"
-#define AXTP_SPEC_TAG "spec/v0.8.4"
-#define AXTP_SPEC_COMMIT "e753aef6779ffee759382fff6ff128f9c4409f0c"
+#define AXTP_RUNTIME_VERSION "0.8.5.0"
+#define AXTP_SPEC_VERSION "0.8.5"
+#define AXTP_SPEC_TAG "spec/v0.8.5"
+#define AXTP_SPEC_COMMIT "5397a57d6784effffd175f3026db0867ad5347f6"
 #define AXTP_GENERATOR_NAME "@axtp/c-runtime-generator"
 #define AXTP_GENERATOR_VERSION "1.0.0"
-#define AXTP_GENERATED_AT "2026-06-20T04:54:17Z"
+#define AXTP_GENERATED_AT "2026-06-20T12:30:02Z"
 
 #endif
